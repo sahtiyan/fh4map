38d16ef4d2694181bdf785da666aa3342ec13035
sahtiyan <81315836+sahtiyan@users.noreply.github.com>
2021-03-28 00:52:14 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,13 +4,13 @@ games { 'gta5' }
 description "Forza Horizon 4 Minimap"
 author "Akkariin"
 url "https://www.zerodream.net/"
-version "1.2"
+version "1.3"
 
 client_script 'client.lua'
 server_script 'server.lua'
 
-files {
-	"hud.html"
-}
+ui_page 'hud.html'
 
-ui_page "hud.html"
+files {
+	'hud.html'
+}
